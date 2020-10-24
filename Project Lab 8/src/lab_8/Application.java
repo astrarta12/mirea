8cf9364ea676ad3ab5bc6bc5e7ae48354c09593f
icYFTL <savap0@yandex.ru>
2020-10-24 22:16:26 +0300

    commit

---
+++ Project Lab 8/src/lab_8/Application.java
@@ -0,0 +1,16 @@
+package lab_8;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Application {
+
+    public Application() {
+        JFrame frame = new JFrame();
+        frame.add(new ShapePanel(new Dimension(400, 400), 20));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
---
+++ Project Lab 8/src/lab_8/Main.java
@@ -0,0 +1,7 @@
+package lab_8;
+
+public class Main {
+    public static void main(String[] args) {
+        new Application();
+    }
+}
